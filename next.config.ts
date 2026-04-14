43a3f6ee1d00534070fa868ff69689b06ab85bb7
Siddhartha Shakya <sshakya@dons.usfca.edu>
2026-04-13 18:58:52 -0700

    skip eslint during vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   cacheComponents: true,
   skipMiddlewareUrlNormalize: true,
   skipTrailingSlashRedirect: true,
