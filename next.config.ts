f97da92dd863c2b78130c59b6b31f86c4b6a5408
Siddhartha Shakya <sshakya@dons.usfca.edu>
2026-04-13 18:43:29 -0700

    fixing vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/': ['./middleware.js'],
+    },
+  },
   serverExternalPackages: ['apify-client'],
   images: {
     remotePatterns: [
